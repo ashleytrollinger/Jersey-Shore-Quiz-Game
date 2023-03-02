514ba42b98cfbc80789b30984a787d12f4a84777
ashleytrollinger <ashley@trollingers.com>
2023-03-02 16:55:31 -0500

    js files combined into one

--- script.js
+++ script.js
@@ -24,3 +24,63 @@ function startQuiz() {
     homePage.replaceChild(quizScreen, rulesPage);
 
 }
+
+
+
+//Array of questions with objects
+let questions = [
+    {
+        numb: 1,
+        question: "Who calls Angelina a dirty little hamster?",
+        answer: "Mike",
+        options: [
+            "Pauly D",
+            "Vinny",
+            "Snooki",
+            "Mike"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does GTL stand for?",
+        answer: "Gym, tan, laundry",
+        options: [
+            "Guido, tan, lift",
+            "Gym, tan, laundry",
+            "Guido, tan, lift",
+            "Gym, tequila, love"
+        ]
+    },
+    {
+        numb: 3,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 4,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: 5,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },

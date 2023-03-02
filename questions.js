3a21c236c21e045ef7821b25b2f849099eddc487
ashleytrollinger <ashley@trollingers.com>
2023-03-02 13:11:53 -0500

    FIles added and started

---
+++ questions.js
@@ -0,0 +1,56 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who calls Angelina a dirty little hamster?",
+        answer: "Mike",
+        options: [
+            "Pauly D",
+            "Vinny",
+            "Snooki",
+            "Mike"
+        ]
+    },
+    {
+        numb: ,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: ,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: ,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        numb: ,
+        question: "?",
+        answer: "",
+        options: [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
